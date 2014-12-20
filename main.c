9389db465ba61e640f3b6ade61e20ecf2c6fed41
Hyeon Kim <simnalamburt@gmail.com>
2014-12-20 22:17:21 +0900

    Handle input_file1

--- main.c
+++ main.c
@@ -11,5 +11,25 @@ int main(int argc, char** argv) {
     return 1;
   }
 
+  FILE* input;
+  char buffer[256];
+
+  input = fopen(argv[1], "r");
+  fgets(buffer, sizeof buffer, input);
+  while (true) {
+    int first;
+    if (fscanf(input, "%d%*c ", &first) == EOF) { break; };
+    if (fgets(buffer, sizeof buffer, input) == EOF) { break; };
+
+    printf("%d, ", first); // TODO : remove this line
+
+    char* ptr = strtok(buffer, ", \n");
+    while(ptr) {
+      int number = atoi(ptr);
+      printf("%d, ", number); // TODO : remove this line
+      ptr = strtok(NULL, ", \n");
+    }
+    putchar('\n');
+  }
   return 0;
 }
